0dd01e2d3e017c3f15ec2633fedb83eb9a83ff2b
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-19 17:20:52 -0300

    FEAT: seed for users collection

--- src/main/java/com/educandoweb/chroniclebase/config/Config.java
+++ src/main/java/com/educandoweb/chroniclebase/config/Config.java
@@ -1,14 +1,34 @@
 package com.educandoweb.chroniclebase.config;
 
+import com.educandoweb.chroniclebase.entities.User;
+import com.educandoweb.chroniclebase.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
-public class Config implements WebMvcConfigurer {
+public class Config implements WebMvcConfigurer, CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.setUseTrailingSlashMatch(true);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com");
+        User u3 = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(u1, u2, u3));
     }
 }
