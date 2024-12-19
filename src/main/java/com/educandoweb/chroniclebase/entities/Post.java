3bfe26919134d1df335495f791dc9e29e448c4f2
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-19 19:35:35 -0300

    FEAT: comments list to post

--- src/main/java/com/educandoweb/chroniclebase/entities/Post.java
+++ src/main/java/com/educandoweb/chroniclebase/entities/Post.java
@@ -1,11 +1,14 @@
 package com.educandoweb.chroniclebase.entities;
 
 import com.educandoweb.chroniclebase.dto.AuthorDTO;
+import com.educandoweb.chroniclebase.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "posts")
@@ -17,6 +20,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -67,6 +71,10 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
     }
 
     @Override
