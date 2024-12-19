08ef9a60a3d074f5ee916bedd763cf45fe6ff8ea
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-19 19:27:01 -0300

    FEAT: post service

---
+++ src/main/java/com/educandoweb/chroniclebase/services/PostService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.chroniclebase.services;
+
+import com.educandoweb.chroniclebase.entities.Post;
+import com.educandoweb.chroniclebase.repository.PostRepository;
+import com.educandoweb.chroniclebase.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+
+        if (post.isEmpty()) {
+            throw new ObjectNotFoundException("post not found.");
+        }
+
+        return post.get();
+    }
+}
