c22fc6495521e64b45309215c5ec9b8ed7a32689
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-19 17:55:51 -0300

    FEAT: object not found exception

--- src/main/java/com/educandoweb/chroniclebase/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/educandoweb/chroniclebase/services/exceptions/ObjectNotFoundException.java
@@ -1,7 +1,15 @@
 package com.educandoweb.chroniclebase.services.exceptions;
 
+import org.springframework.http.HttpStatus;
+
 public class ObjectNotFoundException extends RuntimeException {
-  public ObjectNotFoundException(String message) {
-    super(message);
-  }
+    private final HttpStatus status = HttpStatus.NOT_FOUND;
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
 }
