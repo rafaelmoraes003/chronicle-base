9966bc0648ac69154c1aecea9c0c323dfda55933
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-19 18:53:57 -0300

    FEAT: seed for posts collection

--- src/main/java/com/educandoweb/chroniclebase/config/Config.java
+++ src/main/java/com/educandoweb/chroniclebase/config/Config.java
@@ -1,6 +1,8 @@
 package com.educandoweb.chroniclebase.config;
 
+import com.educandoweb.chroniclebase.entities.Post;
 import com.educandoweb.chroniclebase.entities.User;
+import com.educandoweb.chroniclebase.repository.PostRepository;
 import com.educandoweb.chroniclebase.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -15,6 +18,9 @@ public class Config implements WebMvcConfigurer, CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
@@ -29,6 +35,23 @@ public class Config implements WebMvcConfigurer, CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com");
         User u3 = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post p1 = new Post(
+                null,
+                Instant.now(),
+                "Trip to New York",
+                "I'm traveling to New York. Cheers!",
+                u1
+        );
+
+        Post p2 = new Post(
+                null,
+                Instant.now(),
+                "Good morning",
+                "Woke up happy today!",
+                u1
+        );
+
         userRepository.saveAll(Arrays.asList(u1, u2, u3));
+        postRepository.saveAll(Arrays.asList(p1, p2));
     }
 }
