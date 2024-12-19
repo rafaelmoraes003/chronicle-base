2557cf9d87492e54841f5840f3aa7c997a6ab229
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-19 17:55:27 -0300

    FEAT: find user by id endpoint

--- src/main/java/com/educandoweb/chroniclebase/resources/UserResource.java
+++ src/main/java/com/educandoweb/chroniclebase/resources/UserResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,13 @@ public class UserResource {
                 .map(u -> new UserDTO(u))
                 .toList();
         return ResponseEntity.status(HttpStatus.OK).body(usersDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        UserDTO userDTO = new UserDTO(user);
+        return ResponseEntity.status(HttpStatus.OK).body(userDTO);
     }
 
 }
