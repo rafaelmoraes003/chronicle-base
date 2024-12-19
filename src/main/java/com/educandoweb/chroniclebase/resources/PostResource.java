a6b9b2d883181e97ee993090ac6f09592bb605aa
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-19 19:27:23 -0300

    FEAT: find post by id endpoint

---
+++ src/main/java/com/educandoweb/chroniclebase/resources/PostResource.java
@@ -0,0 +1,25 @@
+package com.educandoweb.chroniclebase.resources;
+
+import com.educandoweb.chroniclebase.entities.Post;
+import com.educandoweb.chroniclebase.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(post);
+    }
+}
