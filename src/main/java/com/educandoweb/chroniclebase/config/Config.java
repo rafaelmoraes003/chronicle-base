b45dfa2c2371c34559f721f790647fe2b9682e93
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-18 23:01:56 -0300

    CONFIG: set use trailing slash match

---
+++ src/main/java/com/educandoweb/chroniclebase/config/Config.java
@@ -0,0 +1,14 @@
+package com.educandoweb.chroniclebase.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
+    }
+}
