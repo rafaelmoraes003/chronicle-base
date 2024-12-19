8b8748b415791582c435defb68079fcf61557b81
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-18 22:31:31 -0300

    project started

---
+++ src/main/java/com/educandoweb/chroniclebase/ChronicleBaseApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.chroniclebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronicleBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChronicleBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educandoweb/chroniclebase/ChroniclebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.chroniclebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChroniclebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
