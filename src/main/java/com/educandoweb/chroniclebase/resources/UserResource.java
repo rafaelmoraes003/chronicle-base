80730c5a4f8d2e291a42a8797d470c46b6adab3c
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-19 18:11:23 -0300

    FEAT: insert user endpoint

--- src/main/java/com/educandoweb/chroniclebase/resources/UserResource.java
+++ src/main/java/com/educandoweb/chroniclebase/resources/UserResource.java
@@ -6,10 +6,7 @@ import com.educandoweb.chroniclebase.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +32,13 @@ public class UserResource {
         User user = userService.findById(id);
         UserDTO userDTO = new UserDTO(user);
         return ResponseEntity.status(HttpStatus.OK).body(userDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO userData) {
+        User newUser = userService.insert(userData);
+        UserDTO newUserDTO = new UserDTO(newUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUserDTO);
     }
 
 }
