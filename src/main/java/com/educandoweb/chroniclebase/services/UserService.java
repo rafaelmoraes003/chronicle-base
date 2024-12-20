0fc540b559bade2131acd66c993e7050006f4db1
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-19 22:38:56 -0300

    FEAT: save user method

--- src/main/java/com/educandoweb/chroniclebase/services/UserService.java
+++ src/main/java/com/educandoweb/chroniclebase/services/UserService.java
@@ -1,6 +1,7 @@
 package com.educandoweb.chroniclebase.services;
 
 import com.educandoweb.chroniclebase.dto.UserDTO;
+import com.educandoweb.chroniclebase.entities.Post;
 import com.educandoweb.chroniclebase.entities.User;
 import com.educandoweb.chroniclebase.repository.UserRepository;
 import com.educandoweb.chroniclebase.services.exceptions.ObjectNotFoundException;
@@ -42,6 +43,10 @@ public class UserService {
     public User update(String id, UserDTO userData) {
         User user = findById(id);
         updateData(user, userData);
+        return save(user);
+    }
+
+    public User save(User user) {
         return userRepository.save(user);
     }
 
