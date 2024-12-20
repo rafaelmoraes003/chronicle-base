6ae5236e60b2134519ad9650b734b72ef323975d
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-20 01:34:18 -0300

    FEAT: convert string to date method

--- src/main/java/com/educandoweb/chroniclebase/resources/util/URL.java
+++ src/main/java/com/educandoweb/chroniclebase/resources/util/URL.java
@@ -2,10 +2,27 @@ package com.educandoweb.chroniclebase.resources.util;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class URL {
 
     public static String decodeParam(String text) {
         return URLDecoder.decode(text, StandardCharsets.UTF_8);
     }
+
+    public static Instant convertDate(String textDate, Instant defaultValue) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            return LocalDate
+                    .parse(textDate, dtf).atStartOfDay(ZoneOffset.UTC).toInstant();
+        } catch (DateTimeParseException e) {
+            System.out.println(e.getMessage());
+            return defaultValue;
+        }
+    }
+
 }
